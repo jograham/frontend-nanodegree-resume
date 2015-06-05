81ddf87071f2080fae53d8a6485adedf2b3113be
jograham <jograham@BELC02KD1P0FFT3.sea.corp.expecn.com>
2015-06-05 13:10:45 -0700

    Updating files

---
+++ js/resumeBuilder.js
@@ -0,0 +1,19 @@
+// $("#main").append("Jonathan Graham");
+
+// [string].replace([old],[new])
+
+var awesomeThoughts = 
+	"I am Jonathan Graham and I am AWESOME!";
+
+var funThoughts = 
+	awesomeThoughts.replace("AWESOME","FUN");
+
+// var email = "jograham@expedia.com";
+
+// var newEmail = 
+//	email.replace("expedia","gmail");
+
+// console.log(email);
+// console.log(newEmail);
+
+$("#main").append(funThoughts);
